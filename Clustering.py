75ebd5a9fce7b7d583ad4a4a0fc51caf6591ce04
Alon Kwart <alon.kwart@gmail.com>
2018-06-08 18:01:22 +0300

    added some top to bottom declarations
    Signed-off-by: Alon Kwart <alon.kwart@gamil.com>

--- Clustering.py
+++ Clustering.py
@@ -2,6 +2,8 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import sklearn
+
 import Consts
 from time import time, strftime
 from pandas import read_csv
@@ -60,5 +62,73 @@ class Clustering:
                              metrics.adjusted_rand_score(self.dict_dfs_np[Consts.FileSubNames.Y_TRAIN], y_pred)))
 
 
+    def load_test_set(self):
+        """
+        this stage is done earlier when all the data was loaded.
+        :return:
+        """
+        pass
 
 
+    def apply_test_to_estimator(self, estimator):
+        """
+        apply the test set to the chosen estimator.
+        TODO: do we want to pass the estimator or are we going to save it in self.
+        TODO: do we want to return the predicted labels or save them? (I think returning is better)
+        :return:
+        """
+        X_test = self.dict_dfs_pd[Consts.FileSubNames.X_TEST]
+        return estimator.predict(X_test)
+
+    def apply_test_to_cluster(self, cluster_estimator):
+        """
+        apply the test set to the chosen cluster.
+        TODO: do we want to pass the cluster or are we going to save it in self.
+        TODO: do we want to return the predicted labels or save them? (I think returning is better)
+        :return:
+        """
+        X_test = self.dict_dfs_pd[Consts.FileSubNames.X_TEST]
+        return cluster_estimator.predict(X_test)
+
+    def check_performance(self, labels_pred):
+        """
+        how will we check performance?
+            this is something we get to chose. there is no right answer.
+            we need to define what is important to us between the cluster and estimator, and decide on a scoring function
+
+        TODO: Decide on a scoring function (per each compared to label or compared between 2 predictions?)
+        TODO: Decide how params will be passed, inside the class or as parameters.
+        TODO: can we plot some kind of graph?
+        :return:
+        """
+        labels_true = self.dict_dfs_np[Consts.FileSubNames.Y_TEST]
+        return metrics.adjusted_rand_score(labels_true, labels_pred)
+
+    def identify_leading_features_for_party(self, party):
+        """
+        identify the leading features for the given party.
+        TODO: use the data in self or are we going to pass it as a parameter \ argument?
+        :param party:
+        :return:
+        """
+        # https://stackoverflow.com/questions/27491197/scikit-learn-finding-the-features-that-contribute-to-each-kmeans-cluster
+        pass
+
+    def identify_leading_features_for_all_parties(self):
+        """
+        identify the leading features for all the parties.
+        loop over all parties and call identify_leading_features_for_party
+        """
+        pass
+
+    def calculate_a_steady_coalition(self):
+        """
+        this does not need to be automated. better if it is.
+        :return:
+        """
+        pass
+
+
+
+
+gnb = sklearn.naive_bayes.GaussianNB()
